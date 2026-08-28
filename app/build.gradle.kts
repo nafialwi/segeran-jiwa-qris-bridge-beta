b1fcca74561e9670742a0e0d414b4ae8b68a0e9e
nafialwi <nafialwizain@gmail.com>
2026-08-28 14:45:01 +0700

    WP10 Bridge v0.3.0 build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.segeranjiwa.qrisbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1-beta"
+        versionCode = 4
+        versionName = "0.3.0-beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
