6a9dc8c30d09c4b865f5b0b13e017b9c4449c21b
nafialwi <nafialwizain@gmail.com>
2026-08-30 22:29:44 +0700

    Delete app directory

--- app/src/main/java/com/segeranjiwa/qrisbridge/BridgeSession.kt
+++
@@ -1,9 +0,0 @@
-package com.segeranjiwa.qrisbridge
-
-data class BridgeSession(
-    val idToken: String,
-    val refreshToken: String,
-    val uid: String,
-    val username: String,
-    val expiresAtEpochSec: Long
-)
